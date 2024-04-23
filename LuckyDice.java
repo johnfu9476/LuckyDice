f8df0f97294dd3597e8e917e73938dbbf32b1914
johnfu9476 <166563675+johnfu9476@users.noreply.github.com>
2024-04-23 16:57:35 -0500

    Add files via upload

---
+++ LuckyDice.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class LuckyDice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer >>> ");
+        int example = scanner.nextInt();
+        example++;
+        System.out.println("Here is your integer plus one! >>> " + example);
+        
+        //Your code goes here
+    }
+}
---
+++ PracticeProblems.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class PracticeProblems {
+    public static void main(String[] args) {
+        Random random = new Random();
+        /* since we have not gone over using Random
+         * this is an example of using the Random to create an int from 1-100
+         * you may delete if you wish
+         */
+        
+        int example = random.nextInt(100) + 1; 
+        System.out.println(example);
+
+        //Your code goes here
+    }
+}
+
+/* formating >>> 
+ * 
+ * for (int i = [num]; i < [num]; i++) {
+ * 
+ * }
+ * 
+ * while ([conditional statement]) {
+ * 
+ * }
+ * 
+ * int[] variableName = new int[length of Array];
+ * 
+ * System.out.println("text");
+ * 
+ * do {
+ * 
+ * } while(conditional statement);
+*/
