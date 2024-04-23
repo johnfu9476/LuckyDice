04883e8dd393fd7306250c48bdf40096758e1262
johnfu9476 <166563675+johnfu9476@users.noreply.github.com>
2024-04-23 17:17:53 -0500

    Add files via upload

--- LuckyDice.java
+++ LuckyDice.java
@@ -4,11 +4,16 @@ public class LuckyDice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter an integer >>> ");
+        /* this is a new Class called Scanner, which helps us take in User Input
+         * try running this code and seeing how it works, you will need it to complete the lab
+         * you may delete the example if you wish, we will learn more about the Scanner in the future
+         */
+
+        System.out.print("Enter an integer >>> "); //make sure you prompt the user and ask what you want!
         int example = scanner.nextInt();
         example++;
         System.out.println("Here is your integer plus one! >>> " + example);
-        
+
         //Your code goes here
     }
 }
